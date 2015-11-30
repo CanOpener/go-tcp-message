ddf0d1a6d7893fcde7eaf6eb5957866feb51cb04
Mladen Kajic <mladenkajic96@gmail.com>
2015-11-30 22:05:50 +0000

    conn constructor and fatallog functions

--- gtm.go
+++ gtm.go
@@ -9,9 +9,30 @@ type conn struct {
 	netcon            net.Conn
 	IncommingMessages chan *[]byte
 	InfoChan          chan int
+	internalComsChan  chan int
 	outgoingMessages  chan *[]byte
 	MessagesSent      int
 	MessagesReceived  int
 	ReaderListening   bool
 	WriterListening   bool
+}
+
+func NewConnection(con net.Conn) conn {
+	return conn{
+		netcon:            con,
+		IncommingMessages: make(chan *[]byte, 100),
+		InfoChan:          make(chan int, 5),
+		internalComsChan:  make(chan int, 5),
+		outgoingMessages:  make(chan *[]byte, 100),
+		MessagesSent:      0,
+		MessagesReceived:  0,
+		ReaderListening:   false,
+		WriterListening:   false,
+	}
+}
+
+func fatalLog(v ...interface{}) {
+	// later this will use a custom first class
+	// logging function specified by the user
+	log.Fatalln(v)
 }
